15534db34cf60cf8cb79b4961139e09250ee2347
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-04-13 18:17:59 -0300

    begin

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Header from './components/Header/index'
+import './styles.css'
+
+const App = () => {
+  <div className="App">
+    <Header/>
+  </div>
+
+}
+export default App
