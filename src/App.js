ea41148c5ba71d1a94367c43cc48ac755266721b
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-04-14 18:00:21 -0300

    end-t

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Routes from './routes'
+
 import Header from './components/Header/index'
 import './styles.css'
 
 const App = () => {
   <div className="App">
-    <Header/>
+    <Header />
+    <Routes />
   </div>
 
 }
